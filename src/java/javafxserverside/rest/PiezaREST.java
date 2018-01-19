133d3b2addeb8d2d630614512855ade7728f0ea9
clsantos97 <clsantos.edu@gmail.com>
2018-01-19 13:10:50 +0100

    Path corrections.

--- src/java/javafxserverside/rest/PiezaREST.java
+++ src/java/javafxserverside/rest/PiezaREST.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javafxserverside.rest;
 
 import java.util.List;
@@ -137,5 +132,4 @@ public class PiezaREST {
         logger.info("PiezaREST: <" + piezaesList.size() + "> records found.");
         return piezaesList;
     }
-
 }
