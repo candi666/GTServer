5092358ba0e81ace09e7d5860a04e77e417c5145
carlos <clsantos.edu@gmail.com>
2018-01-24 22:26:48 +0100

    Minor adjustments

--- src/java/javafxserverside/entity/Factura.java
+++ src/java/javafxserverside/entity/Factura.java
@@ -49,7 +49,7 @@ import javax.xml.bind.annotation.XmlRootElement;
     )    
 })
 @XmlRootElement
-public class Factura implements Serializable {
+public class Factura implements Serializable{
 
     private static final long serialVersionUID = 1L;
     @Id
